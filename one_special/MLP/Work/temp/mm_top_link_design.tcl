7274193aeee1f94bd9b04e08ba72c2e0328a6e58
JinmingZhuang <m13688033757@163.com>
2022-10-25 13:59:59 -0700

    Delete Work

--- one_special/MLP/Work/temp/mm_top_link_design.tcl
+++
@@ -1,5 +0,0 @@
-set_param project.enableUnifiedAIEFlow true
-add_files ././Work/temp/mm_top_aie_full_netlist.aiexn
-add_files ././Work/temp/mm_top_aie_constraints_for_placer.aiecst
-add_files ././Work/temp/mm_top_aie_routed.aiesol
-link_design -part xcvc1902-vsva2197-2MP-e-S
